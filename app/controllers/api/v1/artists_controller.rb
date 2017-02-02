83fb0a9d9d666f91c178d9409eb0522cfe5da229
IlkhamGaysin <ilkham.gaysin@flatstack.com>
2017-02-02 15:36:16 +0300

    Add update faborite column

--- app/controllers/api/v1/artists_controller.rb
+++ app/controllers/api/v1/artists_controller.rb
@@ -2,9 +2,21 @@ module Api
   module V1
     class ArtistsController < ApplicationController
       expose :artists, -> { Artist.all }
+      expose :artist
 
       def index
         respond_with artists
+      end
+
+      def update
+        artist.update(artist_params)
+        respond_with artist
+      end
+
+      private
+
+      def artist_params
+        params.require(:artist).permit(:favorite)
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     constraints subdomain: 'api' do
       scope module: 'api' do
         namespace :v1 do
-          resources :artists, only: [:index]
+          resources :artists, only: [:index, :update]
         end
       end
     end
