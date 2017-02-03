eead3ae48575bb80f898ba209b98fbbede760bb4
IlkhamGaysin <ilkham.gaysin@flatstack.com>
2017-02-03 04:49:26 +0300

    Add explanation for test case

--- spec/acceptance/api/v1/artists_spec.rb
+++ spec/acceptance/api/v1/artists_spec.rb
@@ -8,7 +8,9 @@ resource "Artists" do
   subject(:response) { json_response_body }
 
   get "api/v1/artists" do
-    example_request "responds with 200" do
+    example_request "fethcing all data from root" do
+      explanation "This method fetch all artists"
+
       expect(response_status).to eq 200
       expect(response).to be
       expect(response.size).to eq 2
