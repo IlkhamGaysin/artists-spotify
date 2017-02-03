69e8d3efd79fb305eb450844c343d656f35ae3bc
IlkhamGaysin <ilkham.gaysin@flatstack.com>
2017-02-03 05:12:19 +0300

    Add new tests

--- spec/acceptance/api/v1/artists_spec.rb
+++ spec/acceptance/api/v1/artists_spec.rb
@@ -3,17 +3,36 @@ require "rails_helper"
 resource "Artists" do
   header "Accept", "application/json"
 
-  let!(:artists) { create_list(:artist, 2) }
-
   subject(:response) { json_response_body }
 
-  get "api/v1/artists" do
-    example_request "fethcing all data from root" do
-      explanation "This method fetch all artists"
+  context "whe fetch artists list" do
+    let!(:artists) { create_list(:artist, 2) }
 
-      expect(response_status).to eq 200
-      expect(response).to be
-      expect(response.size).to eq 2
+    get "api/v1/artists" do
+      example_request "fethcing all data" do
+        explanation "This method fetch all artists"
+
+        expect(response_status).to eq 200
+        expect(response).to be
+        expect(response.size).to eq 2
+      end
+    end
+  end
+
+  context "when mark an artist" do
+    let!(:artist) { create(:artist) }
+
+    put "api/v1/artists/:id" do
+      let!(:id) { artist.id }
+
+      parameter :favorite, true, required: true, scope: :artist
+
+      example_request "marking as a favorite", artist: { favorite: true } do
+        explanation "This method mark as favorite an artist"
+
+        expect(response_status).to eq 204
+        expect(artist.reload.favorite).to eq true
+      end
     end
   end
 end
