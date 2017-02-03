498748cecf1f48f1fea31e7bd6107faecc8ad9e7
IlkhamGaysin <ilkham.gaysin@flatstack.com>
2017-02-04 00:50:16 +0300

    Make root path as for API docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   scope defaults: { format: :json } do
-    root to: "api/v1/artists#index"
-
     namespace :api do
       namespace :v1 do
         resources :artists, only: %i(index update)
