1f180d6dafec7b87f9cf727875d38ed9557f08d1
IlkhamGaysin <ilkham.gaysin@flatstack.com>
2017-02-03 08:16:33 +0300

    Correct quality

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -2,15 +2,13 @@ require "rails_helper"
 
 describe Artist do
   describe "validations" do
-    subject { FactoryGirl.create(:artist) }
+    it { expect(artist).to validate_presence_of(:name) }
+    it { expect(artist).to validate_presence_of(:href) }
+    it { expect(artist).to validate_presence_of(:spotify_id) }
+    it { expect(artist).to validate_presence_of(:external_urls) }
 
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:href) }
-    it { is_expected.to validate_presence_of(:spotify_id) }
-    it { is_expected.to validate_presence_of(:external_urls) }
-
-    it { is_expected.to validate_uniqueness_of(:href) }
-    it { is_expected.to validate_uniqueness_of(:spotify_id).case_insensitive }
-    it { is_expected.to validate_uniqueness_of(:external_urls) }
+    it { expect(artist).to validate_uniqueness_of(:href) }
+    it { expect(artist).to validate_uniqueness_of(:spotify_id).case_insensitive }
+    it { expect(artist).to validate_uniqueness_of(:external_urls) }
   end
 end
